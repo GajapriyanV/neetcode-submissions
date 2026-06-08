e7692e3b313c1dfec1797a9d2cb05c5701149132
GajapriyanV <92266980+GajapriyanV@users.noreply.github.com>
2026-06-08 18:43:24 -0400

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        deepCopy = {}
+
+        def dfs(node):
+            if node.val in deepCopy:
+                return deepCopy[node.val]
+
+            copy = Node(node.val)
+            deepCopy[node.val] = copy
+            
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            
+            return deepCopy[node.val]
+        
+        return dfs(node) if node else None
+
+        
