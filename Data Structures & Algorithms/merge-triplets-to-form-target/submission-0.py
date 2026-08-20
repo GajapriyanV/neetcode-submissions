a88ffa6fd7983daa676154c1562ce5e32d12a9a0
GajapriyanV <92266980+GajapriyanV@users.noreply.github.com>
2026-08-20 16:24:44 -0400

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+
+        a = b = c = False
+
+        for triplet in triplets:
+            a1, b1, c1 = triplet
+
+            if a1 == target[0] and b1 <= target[1] and c1 <= target[2]:
+                a1 = True
+            if a1 <= target[0] and b1 == target[1] and c1 <= target[2]:
+                b1 = True
+            if a1 <= target[0] and b1 <= target[1] and c1 == target[2]:
+                c1 = True
+        
+        return (a1 and b1 and c1)
+        
+        
