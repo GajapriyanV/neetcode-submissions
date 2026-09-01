e36b11f8127b850eaa8129c52311d7f613e5a7f4
GajapriyanV <92266980+GajapriyanV@users.noreply.github.com>
2026-09-01 10:27:04 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        occur = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in occur:
+                ans = occur[diff]
+                return [i, ans]
+            else:
+                occur[diff] = i
+        
+        return  0
