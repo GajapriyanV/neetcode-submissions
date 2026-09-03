3a86377672a3308937343d997646bd5d01ce0c51
GajapriyanV <92266980+GajapriyanV@users.noreply.github.com>
2026-09-03 14:14:23 -0400

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        i = len(digits) - 1
+        carry = 1
+        while carry and i >= 0:
+            carry = (digits[i] + 1) // 10
+            digits[i] = (digits[i] + 1) % 10
+        
+        if carry:
+            digits = [1] + digits
+
+        return digits
