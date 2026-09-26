2063548f0e7d436ed7244204de00edefaa2d0add
GajapriyanV <92266980+GajapriyanV@users.noreply.github.com>
2026-09-25 18:18:25 -0700

    Add: design-browser-history - submission-7

---
+++ Data Structures & Algorithms/design-browser-history/submission-7.py
@@ -0,0 +1,25 @@
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.browser = [homepage]
+        self.idx = 0
+
+    def visit(self, url: str) -> None:
+        # Remove any forward history
+        self.browser = self.browser[:self.idx + 1]
+
+        self.browser.append(url)
+        self.idx += 1
+
+    def back(self, steps: int) -> str:
+        if self.idx - steps > 0:
+            return self.browser[self.idx]
+        else:
+            return self.browser[0]
+        
+
+    def forward(self, steps: int) -> str:
+        if self.idx + steps <= len(self.browser) - 1:
+            return self.browser[self.idx + steps]
+        else:
+            return self.browser[len(self.browser) - 1]
